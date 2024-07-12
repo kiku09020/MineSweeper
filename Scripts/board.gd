385208009eed3b939a935d17fe64ec11138c9779
KK09020 <kiku09020@gmail.com>
2024-07-12 10:25:27 +0900

    爆弾セット処理など

--- Scripts/board.gd
+++ Scripts/board.gd
@@ -10,6 +10,9 @@ extends Panel
 @export var board_size = 4
 
 #*--------------- Fields ---------------*
+var tiles: Array = []
+
+var is_generated_tiles = false
 
 #*--------------- Signals ---------------*
 
@@ -36,9 +39,54 @@ func setup_container():
 
 ## タイル生成
 func generate_tiles():
+	var current_tile_position = Vector2i.ZERO
+
 	for i in range(board_size * board_size):
+		current_tile_position = Vector2i(i % board_size, i / board_size)
+
 		var tile = tile_packed_scene.instantiate()
 		container.add_child(tile)
-		tile.generated()
+		tile.generated(current_tile_position)
+		tiles.append(tile)
+		
+		# はじめにクリックされたタイミングで、爆弾をセット
+		tile.on_button_clicked.connect(func(): set_mines_on_tiles())
 
+#------------------------------------------------------------
+
+## 爆弾をセット
+func set_mines_on_tiles():
+	if is_generated_tiles: return
+
+	for tile in tiles:
+		# 爆弾セット
+		tile.set_mine()
+
+	for tile in tiles:
+		# タイルの周りの爆弾数をカウント
+		set_mines_around_count(tile)
+
+	is_generated_tiles = true
 	pass
+
+## タイルの周りの爆弾数をカウント
+func set_mines_around_count(tile: Tile):
+	var _position = tile.tile_position
+	var count = 0
+
+	for i in range( - 1, 2):
+		for j in range( - 1, 2):
+			var target_position = _position + Vector2i(i, j)
+			# 範囲外の場合はスキップ
+			if target_position.x < 0 or target_position.x >= board_size: continue
+			if target_position.y < 0 or target_position.y >= board_size: continue
+			if target_position == _position: continue # 自分自身はスキップ
+
+			# 目標タイルが爆弾の場合、カウント
+			var target_tile = tiles[target_position.y * board_size + target_position.x]
+			if target_tile.is_mine:
+				count += 1
+	
+	tile.set_mine_count(count)
+
+#------------------------------------------------------------
--- Scripts/tile.gd
+++ Scripts/tile.gd
@@ -3,21 +3,54 @@ extends Panel
 class_name Tile
 
 #*--------------- Extported Fields ---------------*
+@export_group("Nodes")
 @export var button: Button
+@export var label: Label
+
+@export_group("Properties")
+@export var mine_rate = .2
 
 #*--------------- Fields ---------------*
+var tile_position: Vector2i
+
 ## クリアフラグ
 var is_cleared = false
 
+## 爆弾フラグ
+var is_mine = false
+
 #*--------------- Signals ---------------*
+signal on_button_clicked
 
 #*--------------- Methods ---------------*
-func generated():
+## 生成処理
+func generated(_position: Vector2i):
+	tile_position = _position
 	pass
 
-func _on_button_pressed() -> void:
+## 爆弾カウントセット
+func set_mine_count(_count: int):
+	if is_mine: return
+
+	label.text = str(_count)
+
+func set_mine():
+	if is_cleared: return
+
+	is_mine = randf_range(0, 1) < mine_rate
+	if is_mine:
+		label.text = "💣"
+
+## クリック処理
+func clicked():
 	if is_cleared: return
 
 	is_cleared = true
 	button.visible = false
+
+#------------------------------------------------------------
+
+func _on_button_pressed() -> void:
+	clicked()
+	on_button_clicked.emit()
 	pass # Replace with function body.
